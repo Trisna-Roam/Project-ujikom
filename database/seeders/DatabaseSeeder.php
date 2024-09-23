6122940f92b1fdaaa9d933ef103b3fe3eee1ebb4
Trisna-Roam <muhammadtrisna675@gmail.com>
2024-09-23 17:14:25 +0700

    third commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Product;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +20,26 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Product::create([
+            'title' => 'Irodori',
+            'artist' => 'KanoeRana',
+            'length' => '3 menit 1 detik',
+            'description' => 'opening call of the night'
+        ]);
+
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('12345'),
+            'level' => 'admin'
+         ]);
+
+         User::create([
+            'name' => 'user',
+            'email' => 'user@example.com',
+            'password' => bcrypt('12345'),
+            'level' => 'user'
+         ]);
     }
 }
